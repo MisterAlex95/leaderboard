b6cee33b35146d5ca397b1a786e7171c7bb1ff6f
alex.dana@epitech.eu <alex.dana@epitech.eu>
2019-02-17 14:11:44 +0100

    fix rank

--- controllers/api/rank.js
+++ controllers/api/rank.js
@@ -7,7 +7,7 @@ router.get('/:playerName', function (req, res) {
   const playerName = req.params.playerName;
 
   db.sequelize
-    .query(`SELECT *, (SELECT COALESCE(MAX(id)+1, 0) FROM Players) as total FROM (SELECT *, RANK() OVER (ORDER BY score DESC) rank FROM Players) WHERE name = '${playerName}'`, {
+    .query(`SELECT *, (SELECT COALESCE(MAX(id), 0) FROM Players) as total FROM (SELECT *, RANK() OVER (ORDER BY score DESC) rank FROM Players) WHERE name = '${playerName}'`, {
       model: Player,
     })
     .then((players) => {
