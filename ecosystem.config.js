fb7f6daa9a8486c851a7abab77003e73c14a72f3
Alex DANA <alex.dana@milky-interactive.com>
2019-02-19 13:36:42 +0100

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'deploy',
+      host : 'alexdana.me',
+      ref  : 'origin/master',
+      repo : 'git@github.com:misteralex95/leaderboard.git',
+      path : '/home/deploy/leaderboard',
+      'post-deploy' : '. ~/.bashrc && npm install && pm2 reload ecosystem.config.js --env production && npm run bundle',
+      env  : {
+        NODE_ENV: 'production',
+      }
+    },
+    staging : {
+      user : 'deploy',
+      host : 'alexdana.me',
+      ref  : 'origin/develop',
+      repo : 'https://github.com/MisterAlex95/leaderboard.git',
+      path : '/home/deploy/leaderboard',
+      'post-deploy' : '. ~/.bashrc && npm install && pm2 reload ecosystem.config.js --env staging && npm run bundle',
+      env  : {
+        NODE_ENV: 'staging',
+      }
+    }
+  }
+};
