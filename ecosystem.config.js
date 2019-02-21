8a248d48fe924c23d10fe445d1af8c25379edba4
alex.dana@epitech.eu <alex.dana@epitech.eu>
2019-02-21 01:58:49 +0100

    test deploy pm2 and docker

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,32 +3,33 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps : [],
+  apps: [],
 
   /**
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy : {
-    production : {
-      user : 'deploy',
-      host : 'alexdana.me',
-      ref  : 'origin/master',
-      repo : 'git@github.com:misteralex95/leaderboard.git',
-      path : '/home/deploy/leaderboard',
-      'post-deploy' : '. ~/.bashrc && npm install && pm2 reload ecosystem.config.js --env production && npm run bundle',
-      env  : {
+  deploy: {
+    production: {
+      user: 'deploy',
+      host: 'alexdana.me',
+      ref: 'origin/master',
+      repo: 'git@github.com:misteralex95/leaderboard.git',
+      path: '/home/deploy/leaderboard',
+      'post-deploy': '. ~/.bashrc &&  docker build -t leaderboard . && docker run -d --net redis-node -p 3001:3001 --name instance_node leaderboard',
+      // 'post-deploy' : '. ~/.bashrc && npm install && pm2 reload ecosystem.config.js --env production && npm run bundle',
+      env: {
         NODE_ENV: 'production',
       }
     },
-    staging : {
-      user : 'deploy',
-      host : 'alexdana.me',
-      ref  : 'origin/develop',
-      repo : 'git@github.com:misteralex95/leaderboard.git',
-      path : '/home/deploy/leaderboard',
-      'post-deploy' : '. ~/.bashrc && npm install && pm2 reload ecosystem.config.js --env staging && npm run bundle',
-      env  : {
+    staging: {
+      user: 'deploy',
+      host: 'alexdana.me',
+      ref: 'origin/develop',
+      repo: 'git@github.com:misteralex95/leaderboard.git',
+      path: '/home/deploy/leaderboard',
+      'post-deploy': '. ~/.bashrc && npm install && pm2 reload ecosystem.config.js --env staging && npm run bundle',
+      env: {
         NODE_ENV: 'staging',
       }
     }
