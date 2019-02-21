65ac2f152f990ecd3ec9974e0dbe8c20f448595c
alex.dana@epitech.eu <alex.dana@epitech.eu>
2019-02-21 01:49:11 +0100

    edit env on development mode

--- redis/index.js
+++ redis/index.js
@@ -1,5 +1,8 @@
+if (!process.env.NODE_ENV) process.env.NODE_ENV = 'development';
+if (process.env.NODE_ENV === 'development') require('dotenv').config();
+
 module.exports = require('redis').createClient({
-  host: "instance_redis.redis-node",
+  host: process.env.REDIS_URL || "instance_redis.redis-node",
   detect_buffers: true
 });
 
